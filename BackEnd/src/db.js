4de569f94d62a894770492667fc312b169fa897a
Fr4nkVil4nuev4 <20250827@ricaldone.edu.sv>
2025-11-16 20:06:25 -0600

    add ➕: add dotenv for environment variable management and update database connection

--- BackEnd/src/db.js
+++ BackEnd/src/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose"
+import "dotenv/config"
 
 export const connectDB = async () => {
 
 try {
-await mongoose.connect(MONGO_URL)
+await mongoose.connect(process.env.SECRET_KEY_DB)
 console.log(">>> Database connected successfully")
 }
 catch (error) {
